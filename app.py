2f46023b0baf1207666334ee75daf3d2a7e61f02
yoginayak <38529406+yoginayak@users.noreply.github.com>
2024-01-31 15:33:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-# import time
-# import os
-
-# while True:
-#     name = os.environ.get('NAME', 'World')
-#     print(f'Hello, {name}!')
-#     time.sleep(5)  # Sleep for 5 seconds, simulating a continuous service
-
 import time
 #import os
 while True:
